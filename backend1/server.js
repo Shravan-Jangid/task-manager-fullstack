3bd32ba5ac54a41de7f39b3ef1c68306b310b383
Shravan Jangid <jangidshravan2005@gmail.com>
2026-03-14 18:43:46 +0530

    Serve React with Express

--- backend1/server.js
+++ backend1/server.js
@@ -1,15 +1,23 @@
 const express = require("express");
-const cors = require("cors");
+const path = require("path");
 
 const app = express();
-app.use(cors());
+
+app.use(express.json());
 
 app.get("/api/data", (req, res) => {
   res.json({ message: "Hello from backend 🚀" });
 });
 
-const PORT = 5000;
+/* Serve React build */
+app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+});
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log("Server running on port " + PORT);
 });
